e9ff533239a38bce99bdb2e9b62c9c352cdfad69
Abhay Desai <47878872+abhaydesai76@users.noreply.github.com>
2020-04-01 16:27:36 +0400

    Adding FrontEnd Application Code to GIT Repository to deploy it to Heroku

---
+++ build/precache-manifest.9add962a87da5ea6af6ceb770de152ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51496f3a4b3a48ebb06063487c5c63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee2e9aa168e99263699",
+    "url": "/static/css/main.722eadc0.chunk.css"
+  },
+  {
+    "revision": "1e0330594198a2861773",
+    "url": "/static/js/2.c7745894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7745894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee2e9aa168e99263699",
+    "url": "/static/js/main.f09ae02e.chunk.js"
+  },
+  {
+    "revision": "029a043923a7aae3f7c5",
+    "url": "/static/js/runtime-main.65397594.js"
+  }
+]);
